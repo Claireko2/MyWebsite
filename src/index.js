c46dc680eb61274fb77278bcc936dbc7d16a44ed
Claireko <daiginger@cycu.org.tw>
2025-08-28 19:10:40 -0700

    update link

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    />
+
 
     <App />
   </React.StrictMode>
